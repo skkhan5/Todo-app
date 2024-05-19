cd825736c265b28b614f18556561a0ada2d7769d
skkhan5 <yousafzai5588@gmail.com>
2024-05-20 03:37:28 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let input = document.getElementById("inp")
+let text = document.querySelector(".text")
+
+
+function Add(){
+    if (input.value == ""){
+    alert("Please Enter Task")
+    }else{
+
+        let newEle = document.createElement("ul")
+        newEle.innerHTML=`${input.value} <i class="fa-solid fa-trash-can"></i>`
+
+        text.appendChild(newEle);
+        input.value ="";
+        
+        newEle.querySelector("i").addEventListener("click", remove);
+        function remove(){
+            newEle.remove()
+        }
+    
+    }
+}
+Add()
